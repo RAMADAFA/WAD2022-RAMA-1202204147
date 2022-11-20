c6c40cd4665a12c6eaa80214fdc9c43cb166b63c
Rama dafa <dbayu9533@gmail.com>
2022-11-20 21:32:40 +0700

    git push

--- Modul 3 Rama/config/connector.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $connect = mysqli_connect("localhost:4306","root","","modul3");
-?>
--- Modul 3 Rama/config/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require '../config/connector.php';
-
-    $id_mobil = $_GET['id_mobil'];
-
-    mysqli_query($connect, "DELETE FROM showroom_rama_table WHERE id_mobil = $id_mobil");
-    header("location: ../pages/ListCar-rama.php");
-?>
